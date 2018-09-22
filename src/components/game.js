6f6473670ff8aef56531f7c552e1db74b66a03d6
Katy Deng <deng2017.xq@gmail.com>
2018-09-21 22:57:18 -0700

    adding random target number for the game

---
+++ src/components/game.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class Game extends React.Component{
+  target = 10 + Math.floor(40* Math.random());
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.target}>{this.target}</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#ddd',
+    flex: 1,
+    paddingTop: 30,
+  },
+  target:{
+    fontSize: 40,
+    backgroundColor: '#aaa',
+    marginHorizontal: 50,
+    textAlign: 'center',
+  },
+});
